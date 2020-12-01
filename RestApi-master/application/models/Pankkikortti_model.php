bd418db4420c327ddcda515701397d0d2819da86
laurrr <59562828+laurrr@users.noreply.github.com>
2020-12-01 17:48:31 +0200

    Update Pankkikortti_model.php
    Lisätty login

--- RestApi-master/application/models/Pankkikortti_model.php
+++ RestApi-master/application/models/Pankkikortti_model.php
@@ -42,5 +42,12 @@ class Pankkikortti_model extends CI_model
       return FALSE;
     }
   }
+	
+	function check_login($numero){
+    $this->db->select('pin');
+    $this->db->from('pankkikortti');
+    $this->db->where('numero',$numero);
+    return $this->db->get()->row('pin');
+  }
 
 }
