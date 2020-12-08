1b606858ddde6aa27572562a4df2a63645c901e5
laurrr <59562828+laurrr@users.noreply.github.com>
2020-12-08 19:49:57 +0200

    Update Login.php

--- RestApi-master/application/controllers/api/Login.php
+++ RestApi-master/application/controllers/api/Login.php
@@ -6,7 +6,7 @@ class Login extends CI_Controller {
         $this->load->model('Login_model');
         $numero=$this->input->get('numero');
         $plaintext_pin=$this->input->get('pin');
-        $encrypted_pin=$this->login_model->check_login($numero);
+        $encrypted_pin=$this->Login_model->check_login($numero);
 
         if(password_verify($plaintext_pin,$encrypted_pin)){
           $result=true;
