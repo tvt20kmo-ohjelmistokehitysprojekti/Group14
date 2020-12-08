8e2df768cdea73b404f8c373aebe69e70cd03a8d
laurrr <59562828+laurrr@users.noreply.github.com>
2020-12-08 18:38:22 +0200

    Update Login_model.php

--- RestApi-master/application/models/Login_model.php
+++ RestApi-master/application/models/Login_model.php
@@ -4,7 +4,7 @@ class Login_model extends CI_model
 {
   function check_login($numero){
     $this->db->select('pin');
-    $this->db->from('numero');
+    $this->db->from('pankkikortti');
     $this->db->where('numero', $numero);
     return $this->db->get()->row('pin');
   }
